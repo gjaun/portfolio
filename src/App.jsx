6ff8a85d8315d19070ba7afc5ed0da85c2183eb9
gichul0317 <gichul0317@gmail.com>
2024-09-23 20:48:41 -0400

    logo image updated

--- src/App.jsx
+++ src/App.jsx
@@ -7,7 +7,6 @@ function App() {
   return (
     <Router>
       <MainRouter />
-      <h1>Test heading</h1>
     </Router>
   );
 }
--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -1,11 +1,17 @@
 import { Link } from 'react-router-dom';
+import Logo from '../assets/images/portfolio-logo.png';
 
 function Layout(props) {
   return (
     <div>
       <h4>Layout Page</h4>
-      <Link to="/">Home</Link>
-      <Link to="/about">About</Link>
+      <nav>
+        <Link to="/">
+          <img src={Logo} alt="portfolio page logo" />
+        </Link>
+        <Link to="/">Home</Link>
+        <Link to="/about">About</Link>
+      </nav>
     </div>
   );
 }
